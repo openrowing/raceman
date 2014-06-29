8fc626ed8cedbe0c8f0a8cd83e933df860c23e5a
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2014-06-29 14:29:03 +0200

    Added first real resource `/events`
    `/events` lists all nodes in database. Detail resource `/events/:id` shows everything neo4j knows for the given node.
    
    Learned a lot. Done even more wrong properly.

---
+++ src/events_resource.erl
@@ -0,0 +1,59 @@
+-module(events_resource).
+
+-export([init/1, to_html/2, content_types_provided/2, resource_exists/2]).
+
+-include_lib("webmachine/include/webmachine.hrl").
+
+-record(context, {neo, action, event}).
+
+init(_Config) ->
+	Neo = neo4j:connect([{base_uri, <<"http://localhost:7474/db/data/">>}]),
+    {ok, #context{neo = Neo}}.
+
+content_types_provided(ReqData, Context) ->
+    {[{"text/html", to_html}], ReqData, Context}.
+
+resource_exists(ReqData, Context) ->
+	case lists:keyfind(action_or_id, 1, wrq:path_info(ReqData)) of
+		{action_or_id, "new"} ->
+			{true, ReqData, Context#context{action=new}};
+		{action_or_id, Str} ->
+			case string:to_integer(Str) of
+				{error, _Reason} ->
+					{false, ReqData, Context};
+				{Id, _Rest} ->
+					Node = neo4j:get_node(Context#context.neo, Id),
+					case Node of
+						{error, not_found} ->
+							{false, ReqData, Context};
+						_Else ->
+						  {true, ReqData, Context#context{action=show_or_update, event=Node}}
+					end
+			end;
+		false ->
+			{true, ReqData, Context#context{action=index_or_create}};
+		_Else ->
+			{false, ReqData, Context}
+	end.		
+
+to_html(ReqData, Context) when Context#context.action == index_or_create ->
+
+    {ok, Events} = neo4j_utils:transform_cypher_result(neo4j:cypher(Context#context.neo, <<"MATCH (n) RETURN n.name AS name, ID(n) AS id LIMIT 100">>)),
+
+    {ok, Content} = events_index_dtl:render([
+	    {events, Events}
+	  ]),
+    {Content, ReqData, Context};
+
+to_html(ReqData, Context) when Context#context.action == show_or_update ->
+    {erlang:iolist_to_binary([<<"<pre>">>, 
+				  mochiweb_html:escape(lists:flatten(io_lib:format("~80p", [Context#context.event]))), 
+				  <<"</pre>">>]), 
+	 ReqData, Context};
+
+to_html(ReqData, Context) when Context#context.action == new ->
+    {<<"TODO">>, ReqData, Context}.
+
+-include_lib("eunit/include/eunit.hrl").
+-ifdef(TEST).
+-endif.
---
+++ src/neo4j_utils.erl
@@ -0,0 +1,43 @@
+-module(neo4j_utils).
+
+-export([transform_cypher_result/1]).
+
+transform_cypher_result([{<<"columns">>, Columns}, {<<"data">>, Data}]) ->
+	{ok, lists:map(fun(Row) -> transform(Columns, Row) end, Data)};
+
+transform_cypher_result([{columns, Columns}, {data, Data}]) ->
+	{ok, lists:map(fun(Row) -> transform(Columns, Row) end, Data)};
+
+transform_cypher_result(_) ->
+	false.
+
+transform([Column|Columns], [Item|Items]) -> 
+	lists:append([{Column, Item}], transform(Columns, Items));
+
+transform([], []) ->
+	[];
+
+transform([_H|_T], []) ->
+	[];
+
+transform([], [_H|_T]) ->
+	[].
+
+-ifdef(TEST).
+
+	-include_lib("eunit/include/eunit.hrl").
+
+	robustness_test() ->
+		?assert(transform_cypher_result([]) =:= false).
+
+	simple_test() ->
+		?assertEqual({ok, [[{1, "foo"}, {2, "bar"}, {3, "baz"}]]},
+					 transform_cypher_result([{columns, [1, 2, 3]}, {data, [["foo", "bar", "baz"]]}])).
+
+	edge_case_test() ->
+		?assertEqual({ok, [[{1, "foo"}]]},
+					 transform_cypher_result([{columns, [1, 2, 3]}, {data, [["foo"]]}])),
+		?assertEqual({ok, [[{1, "foo"}, {2, "bar"}]]},
+					 transform_cypher_result([{columns, [1, 2]}, {data, [["foo", "bar", "baz"]]}])).
+
+-endif.
